ee67c555e18ea22ea3bb62adb578be09a90fd1a1
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-03-26 19:55:03 -0700

    u

---
+++ stylelint.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('stylelint').Config} */
+export default {
+    extends: ['stylelint-config-standard'],
+    rules: {
+        'property-no-vendor-prefix': [
+            true,
+            { ignoreProperties: ['-webkit-user-select', '-webkit-backdrop-filter'] },
+        ],
+        'font-family-name-quotes': null,
+    },
+    overrides: [
+        {
+            files: ['public/**/*.css'],
+            rules: {},
+        },
+    ],
+};
