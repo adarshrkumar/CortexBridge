710b3ac4a395028b0e7c66ec6cb80dc389c401fe
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-03-26 22:22:26 -0700

    u

--- scripts/generate-colors.mjs
+++ scripts/generate-colors.mjs
@@ -15,24 +15,63 @@ const paths = {
 };
 
 function buildCssVars(colors) {
-    const light = Object.entries(colors).map(([name, { light }]) => `    --color-${name}: ${light};`).join('\n');
-    const dark = Object.entries(colors).map(([name, { dark }]) => `        --color-${name}: ${dark};`).join('\n');
-    return `:root {\n${light}\n\n    @media (prefers-color-scheme: dark) {\n${dark}\n    }\n}`;
+    const light = Object.entries(colors)
+        .map(([name, { light }]) => `    --color-${name}: ${light};`)
+        .join('\n');
+    const dark = Object.entries(colors)
+        .map(([name, { dark }]) => `        --color-${name}: ${dark};`)
+        .join('\n');
+        
+    const str =
+`:root {
+${light}
+
+    @media (prefers-color-scheme: dark) {
+${dark}
+    }
+}`
+    return str;
 }
 
 function buildScssVars(colors) {
-    const light = Object.entries(colors).map(([name, { light }]) => `$color-${name}-light: ${light};`).join('\n');
-    const dark = Object.entries(colors).map(([name, { dark }]) => `$color-${name}-dark: ${dark};`).join('\n');
-    const cssLight = Object.entries(colors).map(([name]) => `    --color-${name}: #{$color-${name}-light};`).join('\n');
-    const cssDark = Object.entries(colors).map(([name]) => `        --color-${name}: #{$color-${name}-dark};`).join('\n');
-    const cssVars = `:root {\n${cssLight}\n\n    @media (prefers-color-scheme: dark) {\n${cssDark}\n    }\n}`;
-    return `${light}\n\n${dark}\n\n${cssVars}`;
+    const light = Object.entries(colors)
+        .map(([name, { light }]) => `$color-${name}-light: ${light};`)
+        .join('\n');
+    const dark = Object.entries(colors)
+        .map(([name, { dark }]) => `$color-${name}-dark: ${dark};`)
+        .join('\n');
+    const cssLight = Object.entries(colors)
+        .map(([name]) => `    --color-${name}: #{$color-${name}-light};`)
+        .join('\n');
+    const cssDark = Object.entries(colors)
+        .map(([name]) => `        --color-${name}: #{$color-${name}-dark};`)
+        .join('\n');
+
+    const str =
+`${light}
+
+${dark}
+
+:root {
+${cssLight}
+
+    @media (prefers-color-scheme: dark) {
+${cssDark}
+    }
+}`
+    return str;
 }
 
 export default function run() {
     const colors = JSON.parse(readFileSync(colorsPath, 'utf-8'));
-    paths.css.forEach(p => writeFileSync(join(__dirname, p), buildCssVars(colors) + '\n'));
-    paths.scss.forEach(p => writeFileSync(join(__dirname, p), buildScssVars(colors) + '\n'));
+    paths.css.forEach(p => writeFileSync(
+        join(__dirname, p),
+        `${buildCssVars(colors)}\n`,
+    ));
+    paths.scss.forEach(p => writeFileSync(
+        join(__dirname, p),
+        `${buildScssVars(colors)}\n`,
+    ));
 }
 
 if (process.argv[1] === fileURLToPath(import.meta.url)) {
