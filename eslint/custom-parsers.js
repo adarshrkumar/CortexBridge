1f9963adb11630565497531088abfb8edd4498ce
Adarsh Kumar <86740712+adarshrkumar@users.noreply.github.com>
2026-08-01 16:02:23 -0700

    u

--- eslint/custom-parsers.js
+++ eslint/custom-parsers.js
@@ -1,9 +1,9 @@
 import fs from 'fs';
-import { dirname } from 'path';
+import path from 'path';
 import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename); 
+const __dirname = path.dirname(__filename);
 
 const parserFiles = fs.readdirSync(`${__dirname}/parsers`).filter(f => f.endsWith('.js') && !f.startsWith('_'));
 
